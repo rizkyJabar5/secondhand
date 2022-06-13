3ca4661e42839743673daa32450b9aaa135ddbf6
rizkyJabar5 <rkhamet12@gmail.com>
2022-06-14 01:09:23 +0700

    14/06/2022 | Membuat struktur folder dan konfigurasi spring properties.

---
+++ src/main/java/com/secondhand/ecommerce/SecondHandApplication.java
@@ -0,0 +1,13 @@
+package com.secondhand.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/secondhand/ecommerce/SecondHandEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondhand.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
