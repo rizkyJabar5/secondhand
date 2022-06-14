1225c91771e1f15e2ec7aed04d6d29d18a072235
rizkyJabar5 <rkhamet12@gmail.com>
2022-06-14 23:30:07 +0700

    14/06/2022 | Rename Roles ke AppRoles.

---
+++ src/main/java/com/secondhand/ecommerce/models/entity/AppRoles.java
@@ -0,0 +1,22 @@
+package com.secondhand.ecommerce.models.entity;
+
+import com.secondhand.ecommerce.models.enums.ERole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "app_roles")
+public class AppRoles extends AbstractEntity {
+
+    private ERole roleNames;
+
+}
